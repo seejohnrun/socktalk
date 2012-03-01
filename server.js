74b60f095143e351922e10dd2cfdd0ddaef6c125
John Crepezzi <john.crepezzi@gmail.com>
2012-03-01 00:31:33 -0500

    Moved logging config into server.js

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var fs = require('fs');
+var winston = require('winston');
 var ProxyServer = require('./lib/proxy_server');
 
 // Load the configuration
 var config = JSON.parse(fs.readFileSync('./config.json'));
+
+// Load the logging configuration
+winston.remove(winston.transports.Console);
+var detail, type;
+for (var i = 0; i < config.logging.length; i++) {
+  detail = config.logging[i];
+  type = detail.type;
+  delete detail.type;
+  winston.add(winston.transports[type], detail);
+}
+
 
 // Create a new server and start it
 var server = new ProxyServer(config);

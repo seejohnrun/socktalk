bcb547d216c79fb6f299c14eefee9d9a1fa36fb1
John Crepezzi <john.crepezzi@gmail.com>
2012-03-02 12:53:17 -0500

    Added more tests for configurable server

---
+++ test/configurable_server_test.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var configurable_server = require('../lib/configurable_server');
+
+describe('ConfigurableServer', function () {
+
+  describe('with defaults', function () {
+
+    var config = { host: 'localhost', port: 8888 };
+
+    it('should get back an http server', function () {
+      var server = configurable_server.get(config);
+      assert.equal(server.SNICallback, undefined);
+    });
+
+  });
+  describe('with asking for ssl', function () {
+
+    var config = { host: 'localhost', port: 8888, ssl: true };
+
+    it('should get back an https server', function () {
+      var server = configurable_server.get(config);
+      assert.notEqual(server.SNICallback, undefined);
+    });
+
+  });
+
+});

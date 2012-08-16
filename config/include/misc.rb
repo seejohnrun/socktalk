b71410f1aadf1a645c125c77afe209957020fa70
John Crepezzi <john.crepezzi@gmail.com>
2012-08-16 17:50:31 -0400

    Remove config

--- config/include/misc.rb
+++
@@ -1,9 +0,0 @@
-namespace :misc do  
-  task :symlink_configs, :except => { :no_release => true } do
-    %w(config.json code).each do |f|
-      logger.notice "Symlinking config => #{f}"
-      run "ln -svf #{shared_path}/config/#{f} #{release_path}/#{f}"
-    end
-  end
-end
-
